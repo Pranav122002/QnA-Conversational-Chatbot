f9df59d0231a4a56a5fc55d3c4e96b0677aae12c
Pranav Patil <pranavpatil122002@gmail.com>
2023-10-21 11:25:28 +0530

    temporary history of qeuries

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import axios from "axios";
 function App() {
   const [question, setQuestion] = useState("");
   const [answer, setAnswer] = useState("");
+  const [qaHistory, setQaHistory] = useState([]);
 
   const handleQuestionSubmit = async () => {
     try {
@@ -11,6 +12,10 @@ function App() {
         question: question,
       });
 
+      const newQa = { question, answer: response.data.answer };
+      setQaHistory([...qaHistory, newQa]);
+
+      setQuestion("");
       setAnswer(response.data.answer);
     } catch (error) {
       console.error(error);
@@ -26,7 +31,19 @@ function App() {
         onChange={(e) => setQuestion(e.target.value)}
       />
       <button onClick={handleQuestionSubmit}>Submit</button>
-      <p>Answer: {answer}</p>
+      {/* <p>Answer: {answer}</p> */}
+      <div>
+        <h2>Question-Answer History:</h2>
+        <ul>
+          {qaHistory.map((qa, index) => (
+            <li key={index}>
+              <strong>Q:</strong> {qa.question}
+              <br />
+              <strong>A:</strong> {qa.answer}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
